67aad733f322c5fd67fcf0f56b97652cb89409cb
Avotexs <abderahmanedoubabi@gmail.com>
2025-07-26 13:28:28 +0100

    fix routes conflit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,22 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-<<<<<<< HEAD
 import { ProblemComponent } from './problem/problem-component';
+import { CampagnyComponent } from './campagny/Campagny-Component';
 
 export const routes: Routes = [
   
   { path: '', component: Home },
-  { path: 'problem', component: ProblemComponent }
-];
-=======
-import { CampagnyComponent } from './campagny/Campagny-Component';
-
-export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'campagny', component: CampagnyComponent },
+  { path: 'problem', component: ProblemComponent },
+  { path: 'campagny', component: CampagnyComponent }
 
 ];
-    
-
-
->>>>>>> b0f53cd2e6d82dcd5e3ace2b71cb869a278b711c
