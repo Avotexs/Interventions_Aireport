dc4d7aedbe0ca76d0c90c760a9c2650d629d96b5
Avotexs <abderahmanedoubabi@gmail.com>
2025-08-05 13:34:13 +0100

    fix conflit 2

--- frontend/src/app/solution/solution-component.ts
+++ frontend/src/app/solution/solution-component.ts
@@ -35,7 +35,7 @@ showEditSuccessPopup = false;
 showDeleteSuccessPopup = false;
 solutionToDelete: number|null = null;
   totalItems: number = 0;
-selectedEntity = '';
+selectedEntity = 'solution';
 
   toggleLang() {
    this.langService.toggleLang();
